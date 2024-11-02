73a09edc3e9a80c5c31a3d15f4d3a06fc2c88a6d
edRibeiro <edersonribeiro.ita@gmail.com>
2024-11-02 17:21:44 -0300

    feat: Criando a importação de produtos com fila.

---
+++ app/Console/Commands/importProducts.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Jobs\FecthProductsImportFile;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Http;
+
+class importProducts extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'import:products';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Comando para importar produdos.';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $url = 'https://challenges.coode.sh/food/data/json/index.txt'; // IMPORT_URL
+        $response = Http::get($url);
+        if ($response->successful()) {
+            $conteudo = $response->body();
+            $linhas = explode(PHP_EOL, $conteudo);
+            foreach ($linhas as $fileName) {
+                if (DB::table('files_imported')->where('file_name', '=', $fileName)->doesntExist()) {
+                    FecthProductsImportFile::dispatch($fileName);
+                    DB::table('files_imported')->insert(['file_name' => $fileName]);
+                }
+            }
+        } else {
+            $this->error('Não foi possível ler o arquivo.');
+            return 1;
+        }
+        return 0;
+    }
+}
---
+++ app/Jobs/FecthProductsImportFile.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Helpers\ProductTransform;
+use App\Models\Produto;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Support\Facades\Http;
+
+class FecthProductsImportFile implements ShouldQueue
+{
+    use Queueable;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct(private string $fileName)
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        $gzUrl = "https://challenges.coode.sh/food/data/json/$this->fileName";
+        $response = Http::get($gzUrl);
+        $compressedContent = $response->getBody()->getContents();
+
+        $tempFile = tempnam(sys_get_temp_dir(), 'gzfile');
+        file_put_contents($tempFile, $compressedContent);
+        $gzHandle = gzopen($tempFile, 'r');
+        $index = 0;
+        if ($gzHandle) {
+            while (($line = gzgets($gzHandle)) !== false && $index < 100) {
+                $productData = json_decode($line);
+                Produto::create([
+                    'code' => (int) preg_replace('/\D/', '', $productData->code),
+                    'url' => $productData->url,
+                    'creator' => $productData->creator,
+                    'created_t' => $productData->created_t,
+                    'last_modified_t' => $productData->last_modified_t,
+                    'product_name' => $productData->product_name,
+                    'quantity' => $productData->quantity,
+                    'brands' => $productData->brands,
+                    'categories' => $productData->categories,
+                    'labels' => $productData->labels,
+                    'cities' => $productData->cities,
+                    'purchase_places' => $productData->purchase_places,
+                    'stores' => $productData->stores,
+                    'ingredients_text' => $productData->ingredients_text,
+                    'traces' => $productData->traces,
+                    'serving_size' => $productData->serving_size,
+                    'serving_quantity' => $productData->serving_quantity,
+                    'nutriscore_score' => $productData->nutriscore_score,
+                    'nutriscore_grade' => $productData->nutriscore_grade,
+                    'main_category' => $productData->main_category,
+                    'image_url' => $productData->image_url,
+                    'status' => 'published',
+                    'imported_t' => now()
+                ]);
+                $index++;
+            }
+            gzclose($gzHandle);
+        }
+        unlink($tempFile);
+    }
+}
